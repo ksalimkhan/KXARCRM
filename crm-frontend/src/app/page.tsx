990847cd943ba48fc4482c0a8ef94dd3f40afdbc
Bashiho <basilrdv74@gmail.com>
2025-11-30 09:23:17 -0800

    Small refactoring to remove unused code for signup/login functions

--- crm-frontend/src/app/page.tsx
+++ crm-frontend/src/app/page.tsx
@@ -4,22 +4,20 @@ import {supabase} from "@/app/server/supabaseClient"
 import React from 'react'
 import Login from "@/components/Login"
 import {useState} from "react"
-import {useRouter} from "next/navigation"
-
-const dummyUser = 'admin@KXAR.com';
-const dummyPass = 'password';
 
 export default function Home() {
   const [loginStatus, setLoginStatus] = useState('Pending');
-  const router = useRouter();
 
   async function handleSignIn(){
     const {data} = supabase.auth.onAuthStateChange((event, session) => {
     console.log(event, session)
 
+    // If fails to sign in
     if (event === 'INITIAL_SESSION'){
+      //placeholder, will improve display method in future
       setLoginStatus('Invalid Email or Wrong Password')
     }
+    // If properly signs in
     else if (event === 'SIGNED_IN'){
       window.location.href = 'http://localhost:3000/pages/dashboard'
     }
@@ -27,29 +25,16 @@ export default function Home() {
   }
 
   async function signIn(email: string, password: string){
+    // Calls supabase auth function to sign in
     const {data, error} = await supabase.auth.signInWithPassword({
       email: email,
       password: password
     })
     console.log('Email:', email);
     console.log('Password:', password);
+    // Call helper function to handle sign in event
     handleSignIn()
   }
-  //Validates Login Data
-  const handleLoginSubmit = (username: string, password: string) => {
-    setLoginStatus('Checking Credentials...');
-    if(username === dummyUser && password === dummyPass){
-      {
-        setLoginStatus('Logged In');
-        router.push('/pages/dashboard');
-      }
-    } else {
-      setLoginStatus('Invalid Credentials');
-    }
-    console.log('Login Data Received in Parent:');
-    console.log('Username:', username);
-    console.log('Password:', password);
-  };
 
   return (
     <div>
--- crm-frontend/src/app/signup/page.tsx
+++ crm-frontend/src/app/signup/page.tsx
@@ -4,30 +4,18 @@ import {supabase} from "@/app/server/supabaseClient"
 import React from 'react'
 import SignUp from "@/components/SignUp"
 import {useState} from "react"
-import {useRouter} from "next/navigation"
 
 export default function SignUpPage() {
   const [loginStatus, setLoginStatus] = useState('Pending');
-  const router = useRouter();
 
 
   async function signUp(email: string, password: string, confirmPassword: string){
+    // Calls supabase auth function to signup
       const {data, error} = await supabase.auth.signUp({
           email: email,
           password: password,
       })
   }
-  //Validates Login Data
-  const handleSignUp = (email: string, password: string, confirmPassword: string) => {
-    setLoginStatus('Checking Credentials...');
-    if(password === confirmPassword && password.length > 4){
-      setLoginStatus('Passwords match. Account Created');
-      router.push('/dashboard');
-    } else {
-      setLoginStatus("Passwords don't match");
-    }
-
-  };
 
   return (
     <div>
