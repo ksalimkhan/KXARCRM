654428dae78d6ea0ffaa78de89eae0a5e539f360
alexmaxgamedev <70520824+alexmaxgamedev@users.noreply.github.com>
2025-12-01 17:11:34 -0800

    Update projects.tsx
    also pulls info from supabase

--- src/app/pages/dashboard/tables/projects.tsx
+++ src/app/pages/dashboard/tables/projects.tsx
@@ -1,41 +1,51 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import "./tableStyle.css";
+import { supabase } from '@/app/server/supabaseClient';
 
 export function ShowProjects () {
+
+    const[projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const {data, error } = await supabase.from('projects').select('*');
+
+        if (error) {console.error('Error fetching customers: ', error);}
+        else {setProjects(data);}
+    };
+
     return (
         <div>
-            <h2>Projects</h2>
+        <h2>Payments</h2>
             <table>
                 <thead>
                 <tr>
-                    <th>first_name</th>
-                    <th>last_name</th>
-                    <th>project</th>
-                    <th>status</th>
+                    <th>Project ID</th>
+                    <th>Customer ID</th>
+                    <th>Type</th>
+                    <th>Notes</th>
+                    <th>Status</th>
+                    <th>Due Date</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>Bob</td>
-                    <td>Simon</td>
-                    <td>Logo</td>
-                    <td>Incomplete</td>
-                </tr>
-                <tr>
-                    <td>John</td>
-                    <td>Vargas</td>
-                    <td>Poster</td>
-                    <td>Done</td>
-                </tr>
-                <tr>
-                    <td>Simon</td>
-                    <td>Petrikov</td>
-                    <td>Ice Castle</td>
-                    <td>Done</td>
-                </tr>
+                    {projects.map(projects => (
+                        <tr key={projects.project_id}>
+                            <td>{projects.project_id}</td>
+                            <td>{projects.customer_id}</td>
+                            <td>{projects.type}</td>
+                            <td>{projects.notes}</td>
+                            <td>{projects.status}</td>
+                            <td>{projects.due_date}</td>
+                        </tr>
+                    ))}
                 </tbody>
-                </table>
+            </table>
         </div>
     );
 }
-
-
